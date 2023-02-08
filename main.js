ac1509ed0e40e28b253d81265cf71f8bb11c2bbe
chriscors <70537376+chriscors@users.noreply.github.com>
2023-02-08 10:06:51 -0500

    Adds bootstrap card elements on page

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ searchForm.addEventListener("submit", function (event) {
 function fetchResponse(artist, song) {
   console.log(artist);
   let url = `https://proxy-itunes-api.glitch.me/search?term=${artist}&mediatype=music&attribute=artistTerm`;
+  console.log(url);
   fetch(url, {
     method: "GET",
     headers: {
@@ -46,48 +47,83 @@ function generateHTMLElements(response) {
     let releaseYear = new Date(releaseDate).getFullYear();
     let genre = result.primaryGenreName;
 
-    //create elements
-    let container = document.createElement("div");
-    container.classList.add("col");
+    //create card
+    let card = document.createElement("div");
+    //card.classList.add("col-lg-3", "col-md-6", "mb-4", "gx-6");
+    card.classList.add("card");
 
+    //create outer row
+    let enclosingRow = document.createElement("div");
+    enclosingRow.classList.add("row g-0");
+
+    card.appendChild(enclosingRow);
+
+    //create columns
+    let leftCol = document.createElement("div");
+    leftCol.classList.add("col-md-4");
+    let rightCol = document.createElement("div");
+    rightCol.classList.add("col-md-8");
+
+    //Add columns
+    enclosingRow.append(leftCol, rightCol);
+
+    //make artworkImage
     let artworkImg = document.createElement("img");
-    artworkImg.classList.add("album-art");
+    artworkImg.classList.add("card-img-top");
     artworkImg.src = artworkURL;
     artworkImg.alt = `Album artwork for album ${album}`;
 
-    let trackP = document.createElement("p");
-    trackP.classList.add("line", "track");
-    trackP.innerText = `Song: ${trackName}`;
+    //make playButton
+    let playButton = document.createElement("button");
+    playButton.classList.add("btn btn-default");
+
+    //make play icon
+    let playIcon = document.createElement("img");
+    playIcon.classList.add("justify-content-center", "img-fluid");
+    playIcon.src = "https://icons.getbootstrap.com/assets/icons/play.svg";
+    playIcon.alt = "Play button";
+    //put icon in button
+    playButton.appendChild(playIcon);
+
+    //put image in left column
+    leftCol.append(artworkImg, playButton);
+
+    //make track header
+    let trackH = document.createElement("h5");
+    trackH.classList.add("card-title", "track");
+    trackH.innerText = `${trackName}`;
 
     let albumP = document.createElement("p");
-    albumP.classList.add("line", "album");
+    albumP.classList.add("card-text", "album");
     albumP.innerText = `Album: ${album}`;
 
     let releasedP = document.createElement("p");
-    releasedP.classList.add("line", "released");
+    releasedP.classList.add("card-text", "released");
     releasedP.innerText = `Year: ${releaseYear}`;
 
-    let previewDIV = document.createElement("audio");
-    previewDIV.classList.add("audio");
-    previewDIV.controls = true;
-    previewDIV.innerText = "Your browser does not support the audio element.";
+    // let previewDIV = document.createElement("audio");
+    // previewDIV.classList.add("audio");
+    // previewDIV.controls = true;
+    // previewDIV.innerText = "Your browser does not support the audio element.";
 
-    let previewSource = document.createElement("source");
-    previewSource.classList.add("source");
-    previewSource.src = previewURL;
-    previewSource.type = "audio/mpeg";
-    previewDIV.appendChild(previewSource);
+    // let previewSource = document.createElement("source");
+    // previewSource.classList.add("source");
+    // previewSource.src = previewURL;
+    // previewSource.type = "audio/mpeg";
+    // previewDIV.appendChild(previewSource);
 
     //create node tree
-    container.append(artworkImg, trackP, albumP, releasedP, previewDIV);
+    card.append(trackH, albumP, releasedP); //, previewDIV
+    //every four cards create new row
     if (counter % 4 === 0 && counter > 0) {
       row[row.length] = document.createElement("div");
       row[row.length - 1].classList.add("row");
     }
     counter++;
-    row[row.length].appendChild(container);
+    row[row.length - 1].appendChild(card);
   }
+  //append all rows
   for (const sect of row) {
-    outputDiv.appendChild(container);
+    outputDiv.appendChild(sect);
   }
 }
